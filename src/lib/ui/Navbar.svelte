5c07a70c6a11ecd7b2023fd38369b704fcbd6965
Guido Queiroz <guido98q@gmail.com>
2025-08-20 20:05:46 -0300

    lang buttons + translate splash

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
+	import { getLocale, locales, setLocale, type Locale } from '$lib/paraglide/runtime';
 </script>
 
-{#snippet langBtn(name: string)}
+{#snippet langBtn(name: Locale)}
 	<button
-		onclick={() => console.log(name)}
-		class="flex aspect-square items-center justify-center bg-stone-700 p-2 hover:cursor-pointer"
+		onclick={() => setLocale(name)}
+		disabled={getLocale() == name}
+		class="min-w-10 bg-stone-800 p-1 hover:cursor-pointer disabled:cursor-default disabled:opacity-50"
 	>
 		{name}
 	</button>
 {/snippet}
 
-<div class="flex justify-between bg-black/75">
-	<div class="flex p-4">NAVBAR</div>
+<div class="flex items-center justify-between bg-black/75 p-2">
+	<div class="flex items-center p-2">NAVBAR</div>
 
-	<div class="grid grid-cols-2 items-center gap-2 p-2">
-		{@render langBtn('EN')}
-		{@render langBtn('ES')}
+	<div class="grid grid-cols-2 overflow-hidden rounded-sm">
+		{#each locales as locale}
+			{@render langBtn(locale)}
+		{/each}
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-	console.log('Hello from +page.svelte');
+	import { m } from "$lib/paraglide/messages";
 </script>
 
 <div class="grid h-full grid-rows-[100%_auto]">
 	<div class="flex flex-col items-center justify-center bg-stone-900">
-		<h1 class="text-5xl">Welcome!</h1>
+		<h1 class="text-5xl">{m.tiny_crisp_skate_lead()}</h1>
 
 		<div class="p-10">
-			<h3 class="text-3xl">I'm Guido</h3>
-			<h5 class="text-2xl">fullstack developer</h5>
+			<h3 class="text-3xl">{m.new_free_cougar_leap()}</h3>
+			<h5 class="text-2xl">{m.sharp_trite_bulldog_dash()}</h5>
 		</div>
 	</div>
 
