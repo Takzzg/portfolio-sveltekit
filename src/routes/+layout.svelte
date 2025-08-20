dee334a421fd1a4ff63e7297b0e5bc1742e1db85
guidoq <guido98q@gmail.com>
2025-08-20 00:31:40 +0000

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="w-screen h-screen flex flex-col items-center justify-center text-white bg-[#121212]">
+    {@render children?.()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a class="text-emerald-500" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
