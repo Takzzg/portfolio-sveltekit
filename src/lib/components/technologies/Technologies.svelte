c95fc729c737027c795a083bd196f6b65e24201d
guido98q <guido98q@gmail.com>
2025-09-08 08:05:30 -0300

    refactor technologies

--- src/lib/components/technologies/Technologies.svelte
+++ src/lib/components/technologies/Technologies.svelte
@@ -37,9 +37,42 @@
 	<span class="text-2xl">{tech.name}</span>
 {/snippet}
 
-<div class="flex h-full flex-col bg-teal-900 lg:grid lg:grid-cols-[22rem_1fr]">
+{#snippet techHeader()}
+	<div class="techHeader m-auto flex w-full max-w-[980px] grow gap-8 bg-stone-200 p-4 text-black">
+		{#if selectedTech}
+			<span class="flex w-full items-center gap-4">
+				{@render techLogo(selectedTech, 32)}
+			</span>
+			<span class="flex flex-col gap-2 lg:flex-row">
+				<Button
+					variant="secondary"
+					class="hover:cursor-pointer"
+					onclick={() => window.open(selectedTech?.websiteURL, "_blank")}
+				>
+					<iconify-icon icon="lucide:globe" width="24px" class="h-6 w-6"></iconify-icon>
+					<span>Visit Website</span>
+					<iconify-icon icon="lucide:external-link" width="16px" class="h-4 w-4"></iconify-icon>
+				</Button>
+				<Button
+					variant="secondary"
+					class="hover:cursor-pointer"
+					onclick={() => window.open(selectedTech?.githubURL, "_blank")}
+				>
+					<iconify-icon icon="lucide:github" width="24px" class="h-6 w-6"></iconify-icon>
+					<span>GitHub Repo</span>
+					<iconify-icon icon="lucide:external-link" width="16px" class="h-4 w-4"></iconify-icon>
+				</Button>
+			</span>
+		{:else}
+			<span class="text-2xl">About this Portfolio</span>
+		{/if}
+	</div>
+{/snippet}
+
+{#snippet techList()}
 	<div class="flex flex-col justify-around bg-background py-4">
-		<span class="p-4 text-center text-4xl text-wrap whitespace-break-spaces">This portfolio was made using:</span>
+		<span class="p-4 text-center text-2xl text-wrap whitespace-break-spaces">This portfolio was made using:</span>
+
 		<div class="flex flex-col gap-1">
 			{#each Technologies as tech (tech.id)}
 				<button
@@ -54,40 +87,39 @@
 			{/each}
 		</div>
 	</div>
+{/snippet}
 
-	<div class="flex h-full flex-col items-center overflow-hidden pb-4">
-		<div class="w-full bg-stone-200">
-			<div class="m-auto flex w-full max-w-[980px] items-center gap-8 p-4 text-black lg:flex-row">
-				{#if selectedTech}
-					<span class="flex w-full items-center gap-4">
-						{@render techLogo(selectedTech, 32)}
-					</span>
-					<span class="flex flex-col gap-4 lg:flex-row">
-						<Button
-							variant="secondary"
-							class="hover:cursor-pointer"
-							onclick={() => window.open(selectedTech?.websiteURL, "_blank")}
-						>
-							<iconify-icon icon="lucide:globe" width="24px" class="h-6 w-6"></iconify-icon>
-							<span>Visit Website</span>
-							<iconify-icon icon="lucide:external-link" width="16px" class="h-4 w-4"></iconify-icon>
-						</Button>
-						<Button
-							variant="secondary"
-							class="hover:cursor-pointer"
-							onclick={() => window.open(selectedTech?.githubURL, "_blank")}
-						>
-							<iconify-icon icon="lucide:github" width="24px" class="h-6 w-6"></iconify-icon>
-							<span>GitHub Repo</span>
-							<iconify-icon icon="lucide:external-link" width="16px" class="h-4 w-4"></iconify-icon>
-						</Button>
-					</span>
-				{:else}
-					<span class="text-3xl">About this Portfolio</span>
-				{/if}
-			</div>
-		</div>
+<div class="h-full bg-teal-900 lg:grid lg:grid-cols-[22rem_1fr]">
+	{@render techList()}
+
+	<div class="flex h-full flex-col overflow-hidden pb-4">
+		{@render techHeader()}
 
 		<StyledMarkdown {md} />
 	</div>
 </div>
+
+<style lang="postcss">
+	@reference '@/app.css';
+
+	.techHeader {
+		position: relative;
+	}
+	.techHeader::before,
+	.techHeader::after {
+		@apply bg-stone-200;
+
+		content: " ";
+		position: absolute;
+		width: 1rem;
+		height: 100%;
+		padding: 0;
+		top: 0;
+	}
+	.techHeader::before {
+		right: 100%;
+	}
+	.techHeader::after {
+		left: 100%;
+	}
+</style>
