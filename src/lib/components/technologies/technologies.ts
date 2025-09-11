7087f1927fa423a4a3c0176eead87b7c7f4ccfa3
guido98q <guido98q@gmail.com>
2025-09-11 02:00:20 -0300

    allTechs key type

--- src/lib/components/technologies/technologies.ts
+++ src/lib/components/technologies/technologies.ts
@@ -17,57 +17,57 @@ enum E_Technologies {
 	VERCEL = "vercel",
 }
 
-const AllTechnologies: Record<E_Technologies, I_Technology> = {
-	[E_Technologies.SVELTE_KIT]: {
+const AllTechnologies: Record<keyof typeof E_Technologies, I_Technology> = {
+	SVELTE_KIT: {
 		id: E_Technologies.SVELTE_KIT,
 		icon: "logos:svelte-icon",
 		name: "SvelteKit",
 		github: "sveltejs/kit",
 		website: "https://svelte.dev/",
 	},
-	[E_Technologies.TAILWINDCSS]: {
+	TAILWINDCSS: {
 		id: E_Technologies.TAILWINDCSS,
 		icon: "logos:tailwindcss-icon",
 		name: "TailwindCSS",
 		github: "tailwindlabs/tailwindcss",
 		website: "https://tailwindcss.com/",
 	},
-	[E_Technologies.SHADCN_SVELTE]: {
+	SHADCN_SVELTE: {
 		id: E_Technologies.SHADCN_SVELTE,
 		icon: "vscode-icons:file-type-light-shadcn",
 		name: "shadcn-svelte",
 		github: "huntabyte/shadcn-svelte",
 		website: "https://www.shadcn-svelte.com/",
 	},
-	[E_Technologies.TYPESCRIPT]: {
+	TYPESCRIPT: {
 		id: E_Technologies.TYPESCRIPT,
 		icon: "logos:typescript-icon",
 		name: "TypeScript",
 		github: "microsoft/TypeScript",
 		website: "https://www.typescriptlang.org/",
 	},
-	[E_Technologies.VITE]: {
+	VITE: {
 		id: E_Technologies.VITE,
 		icon: "logos:vitejs",
 		name: "Vite",
 		github: "vitejs/vite",
 		website: "https://vite.dev/",
 	},
-	[E_Technologies.PRETTIER_PLUGIN_SVELTE]: {
+	PRETTIER_PLUGIN_SVELTE: {
 		id: E_Technologies.PRETTIER_PLUGIN_SVELTE,
 		icon: "vscode-icons:file-type-prettier",
 		name: "prettier-plugin-svelte",
 		github: "sveltejs/prettier-plugin-svelte",
 		website: "",
 	},
-	[E_Technologies.ESLINT_PLUGIN_SVELTE]: {
+	ESLINT_PLUGIN_SVELTE: {
 		id: E_Technologies.ESLINT_PLUGIN_SVELTE,
 		icon: "logos:eslint",
 		name: "eslint-plugin-svelte",
 		github: "sveltejs/eslint-plugin-svelte",
 		website: "https://sveltejs.github.io/eslint-plugin-svelte/",
 	},
-	[E_Technologies.VERCEL]: {
+	VERCEL: {
 		id: E_Technologies.VERCEL,
 		icon: "devicon:vercel",
 		name: "Vercel",
@@ -87,8 +87,8 @@ export enum E_Categories {
 	FRONT_END = "front_end",
 	STYLING = "styling",
 	DEVELOPMENT = "development",
-    BACK_END = "back_end",
-    DATA_BASE = "data_base",
+	BACK_END = "back_end",
+	DATA_BASE = "data_base",
 	HOSTING = "hosting",
 }
 
@@ -97,23 +97,19 @@ export const TechCategories: Record<keyof typeof E_Categories, I_Category> = {
 		id: E_Categories.FRONT_END,
 		icon: "lucide:monitor",
 		title: "Front End",
-		techs: [AllTechnologies[E_Technologies.SVELTE_KIT], AllTechnologies[E_Technologies.TYPESCRIPT]],
+		techs: [AllTechnologies.SVELTE_KIT, AllTechnologies.TYPESCRIPT],
 	},
 	STYLING: {
 		id: E_Categories.STYLING,
 		icon: "lucide:paintbrush",
 		title: "Styling",
-		techs: [AllTechnologies[E_Technologies.TAILWINDCSS], AllTechnologies[E_Technologies.SHADCN_SVELTE]],
+		techs: [AllTechnologies.TAILWINDCSS, AllTechnologies.SHADCN_SVELTE],
 	},
 	DEVELOPMENT: {
 		id: E_Categories.DEVELOPMENT,
 		icon: "lucide:code-xml",
 		title: "Development",
-		techs: [
-			AllTechnologies[E_Technologies.VITE],
-			AllTechnologies[E_Technologies.PRETTIER_PLUGIN_SVELTE],
-			AllTechnologies[E_Technologies.ESLINT_PLUGIN_SVELTE],
-		],
+		techs: [AllTechnologies.VITE, AllTechnologies.PRETTIER_PLUGIN_SVELTE, AllTechnologies.ESLINT_PLUGIN_SVELTE],
 	},
 	BACK_END: {
 		id: E_Categories.BACK_END,
@@ -131,17 +127,17 @@ export const TechCategories: Record<keyof typeof E_Categories, I_Category> = {
 		id: E_Categories.HOSTING,
 		icon: "lucide:network",
 		title: "Hosting",
-		techs: [AllTechnologies[E_Technologies.VERCEL]],
+		techs: [AllTechnologies.VERCEL],
 	},
 };
 
 export const Portfolio: I_Technology[] = [
-	AllTechnologies[E_Technologies.SVELTE_KIT],
-	AllTechnologies[E_Technologies.TAILWINDCSS],
-	AllTechnologies[E_Technologies.SHADCN_SVELTE],
-	AllTechnologies[E_Technologies.TYPESCRIPT],
-	AllTechnologies[E_Technologies.VITE],
-	AllTechnologies[E_Technologies.PRETTIER_PLUGIN_SVELTE],
-	AllTechnologies[E_Technologies.ESLINT_PLUGIN_SVELTE],
-	AllTechnologies[E_Technologies.VERCEL],
+	AllTechnologies.SVELTE_KIT,
+	AllTechnologies.TAILWINDCSS,
+	AllTechnologies.SHADCN_SVELTE,
+	AllTechnologies.TYPESCRIPT,
+	AllTechnologies.VITE,
+	AllTechnologies.PRETTIER_PLUGIN_SVELTE,
+	AllTechnologies.ESLINT_PLUGIN_SVELTE,
+	AllTechnologies.VERCEL,
 ];
