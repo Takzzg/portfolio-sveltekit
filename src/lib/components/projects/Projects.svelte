d7da50476ad38e7d13015cf8427f6ddad75880a3
guido98q <guido98q@gmail.com>
2025-09-18 00:34:50 -0300

    projects

--- src/lib/components/projects/Projects.svelte
+++ src/lib/components/projects/Projects.svelte
@@ -1,19 +1,45 @@
 <script lang="ts">
-	import { AllProjects, type I_Project } from "./projects";
+	import IconifyIcon from "../IconifyIcon.svelte";
+	import { AllGames, AllProjects } from "./projects";
 </script>
 
-{#snippet project(project: I_Project)}
-	<div class="flex flex-col border-2 border-red-500">
-		<span>{project.title}</span>
-		<span>{project.description}</span>
-		<a href={project.website} target="_blank">Website</a>
-		<a href={project.github} target="_blank">Github</a>
-	</div>
+{#snippet LinkBtn(text: string, icon: string, link?: string, disabled?: boolean)}
+	<span class="flex items-center gap-2 {disabled ? 'text-secondary' : 'hover:cursor-pointer'}">
+		<IconifyIcon {icon} height="32px" width="32px" />
+		{#if disabled}
+			<span>{text}</span>
+		{:else}
+			<a href={link} target="_blank">{text}</a>
+		{/if}
+	</span>
 {/snippet}
 
 <div>
 	<span>Check out my projects</span>
-	<div class="flex flex-col">
-		{@render project(AllProjects.CONTACTS_APP)}
+	<div class="flex flex-wrap items-start justify-center gap-2 p-2">
+		{#each Object.values(AllProjects) as project (project.id)}
+			<div class="flex w-full max-w-lg flex-col bg-red-500 p-2">
+				<span>{project.title} - {project.year}</span>
+				<span>{project.description}</span>
+				{@render LinkBtn("Github", "logos:github-icon", project.github)}
+				{@render LinkBtn(
+					project.websiteURL ? "Website" : (project.websiteError as string),
+					"lucide:globe",
+					project.websiteURL,
+					project.websiteURL ? false : true,
+				)}
+			</div>
+		{/each}
+	</div>
+
+	<span>I also made some games...</span>
+	<div class="flex flex-wrap items-start justify-center gap-2 p-2">
+		{#each Object.values(AllGames) as game (game.id)}
+			<div class="flex w-full max-w-lg flex-col bg-red-500 p-2">
+				<span>{game.title} - {game.year}</span>
+				<span>{game.description}</span>
+				{@render LinkBtn("Github", "logos:github-icon", game.github)}
+			</div>
+		{/each}
 	</div>
 </div>
--- src/lib/components/projects/projects.ts
+++ src/lib/components/projects/projects.ts
@@ -1,21 +1,138 @@
+const GITHUB_URL = "https://github.com/Takzzg/";
+
 export enum E_Projects {
 	CONTACTS_APP = "contacts-app",
+	MELI_TECH_TEST = "prueba-tecnica-meli",
+	NEXTDOTS_CHALLENGE = "challengeNextDots",
+	PI_HENRY_VIDEOGAMES = "PI-Videogames-Guido",
+	PG_HENRY_FOODFAST = "food-fast",
+	CHALLENGE_25WATTS = "25Watts",
 }
 
-export interface I_Project {
+export type I_Project = {
 	id: string;
-    title: string;
-    description: string;
-	website: string;
+	title: string;
+	year: number;
+	description: string;
 	github: string;
-}
+	websiteURL?: string;
+	websiteError?: string;
+};
 
 export const AllProjects: Record<keyof typeof E_Projects, I_Project> = {
-    CONTACTS_APP: {
-        id: E_Projects.CONTACTS_APP,
-        title: "Contacts App",
-        description: "Exercise project focused on drag-n-drop functionality", 
-        website: "https://contacts-app-guidoq.vercel.app",
-        github: "https://github.com/Takzzg/contacts-app"
-    },
+	CONTACTS_APP: {
+		id: E_Projects.CONTACTS_APP,
+		title: "Contacts App",
+		year: 2022,
+		description: "Next.js app focused on drag-n-drop functionality, part of a technical interview for Front.id",
+		github: GITHUB_URL + E_Projects.CONTACTS_APP,
+		websiteURL: "https://contacts-app-guidoq.vercel.app",
+	},
+	MELI_TECH_TEST: {
+		id: E_Projects.MELI_TECH_TEST,
+		title: "MercadoLibre challenge",
+		year: 2024,
+		description: "Next.js app part of a technical interview for MercadoLibre",
+		github: GITHUB_URL + E_Projects.MELI_TECH_TEST,
+		websiteError: "Taken down - API endpoints no longer available",
+	},
+	NEXTDOTS_CHALLENGE: {
+		id: E_Projects.NEXTDOTS_CHALLENGE,
+		title: "NextDots challenge",
+		year: 2019,
+		description: "React-Native app part of a technical interview for NextDots",
+		github: GITHUB_URL + E_Projects.NEXTDOTS_CHALLENGE,
+	},
+	CHALLENGE_25WATTS: {
+		id: E_Projects.CHALLENGE_25WATTS,
+		title: "25 Watts challenge",
+		year: 2020,
+		description: "Static HTML site using Bootstrap and JQuery, part of a technical interview for 25Watts",
+		github: GITHUB_URL + E_Projects.CHALLENGE_25WATTS,
+		websiteURL: "https://25-watts-challenge-guidoq.vercel.app/",
+	},
+	PI_HENRY_VIDEOGAMES: {
+		id: E_Projects.PI_HENRY_VIDEOGAMES,
+		title: "Inividual Project - SoyHenry",
+		year: 2022,
+		description: "Mono-repo containing an Express.js backend and a React.js frontend",
+		github: GITHUB_URL + E_Projects.PI_HENRY_VIDEOGAMES,
+	},
+	PG_HENRY_FOODFAST: {
+		id: E_Projects.PG_HENRY_FOODFAST,
+		title: "Group Project - SoyHenry",
+		year: 2022,
+		description: "Mono-repo containing an Express.js backend, a React.js frontend and a telegram bot",
+		github: GITHUB_URL + E_Projects.PG_HENRY_FOODFAST,
+	},
+};
+
+export enum E_Games {
+	GRIDDLERS_ULP = "Griddlers-ULP-2025",
+	PYTHON_TETRIS = "Python_Tetris",
+	PYTHON_SNAKE = "Python_Snake",
+	PYTHON_PONG = "Python_Pong",
+	PYTHON_CONNECT_FOUR = "Python_Connect-Four",
+	GODOT_HORDE_SURVIVOR = "Godot_horde-survivor",
+	LIBGDX_PONG = "LibGDX_Pong",
+}
+
+export type I_Game = {
+	id: string;
+	title: string;
+	year: number;
+	description: string;
+	github: string;
+};
+
+export const AllGames: Record<keyof typeof E_Games, I_Game> = {
+	LIBGDX_PONG: {
+		id: E_Games.LIBGDX_PONG,
+		title: "Pong (LibGDX)",
+		year: 2025,
+		description: "Pong game implemented using PyGame",
+		github: GITHUB_URL + E_Games.LIBGDX_PONG,
+	},
+	GODOT_HORDE_SURVIVOR: {
+		id: E_Games.GODOT_HORDE_SURVIVOR,
+		title: "Horde Survivor (Godot)",
+		year: 2025,
+		description: "Horde Survivor game implemented using PyGame",
+		github: GITHUB_URL + E_Games.GODOT_HORDE_SURVIVOR,
+	},
+	GRIDDLERS_ULP: {
+		id: E_Games.GRIDDLERS_ULP,
+		title: "Griddlers ULP",
+		year: 2016,
+		description: "Griddlers game as a console app",
+		github: GITHUB_URL + E_Games.GRIDDLERS_ULP,
+	},
+	PYTHON_TETRIS: {
+		id: E_Games.PYTHON_TETRIS,
+		title: "Tetris (Python)",
+		year: 2021,
+		description: "Tetris game implemented using PyGame",
+		github: GITHUB_URL + E_Games.PYTHON_TETRIS,
+	},
+	PYTHON_SNAKE: {
+		id: E_Games.PYTHON_SNAKE,
+		title: "Snake (Python)",
+		year: 2021,
+		description: "Snake game implemented using PyGame",
+		github: GITHUB_URL + E_Games.PYTHON_SNAKE,
+	},
+	PYTHON_PONG: {
+		id: E_Games.PYTHON_PONG,
+		title: "Pong (Python)",
+		year: 2021,
+		description: "Pong game implemented using PyGame",
+		github: GITHUB_URL + E_Games.PYTHON_PONG,
+	},
+	PYTHON_CONNECT_FOUR: {
+		id: E_Games.PYTHON_CONNECT_FOUR,
+		title: "Connect-Four (Python)",
+		year: 2021,
+		description: "Connect-Four game implemented using PyGame",
+		github: GITHUB_URL + E_Games.PYTHON_CONNECT_FOUR,
+	},
 };
