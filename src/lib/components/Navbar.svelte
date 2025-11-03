dace977baa955d0f4a46b37f53193ae093017e48
guido98q <guido98q@gmail.com>
2025-11-02 21:17:07 -0300

    cleanup

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,8 +8,6 @@
 		DropdownMenuSeparator,
 		DropdownMenuTrigger,
 	} from "$lib/components/ui/dropdown-menu";
-	import { ToggleGroup, ToggleGroupItem } from "$lib/components/ui/toggle-group";
-	import { getLocale, setLocale, type Locale } from "$lib/paraglide/runtime";
 	import type { ContextState } from "@/routes/+layout.svelte";
 	import { userPrefersMode, setMode, resetMode } from "mode-watcher";
 	import { getContext } from "svelte";
@@ -72,37 +70,6 @@
 			</DropdownMenuGroup>
 		</DropdownMenuContent>
 	</DropdownMenu>
-{/snippet}
-
-<!-- (es / en) lang - icon + pretty name -->
-{#snippet langOption(locale: Locale)}
-	{#if locale == "es"}
-		<IconifyIcon icon="flag:ar-1x1" width="16px" height="16px" />
-		Es
-	{:else if locale == "en"}
-		<IconifyIcon icon="flag:gb-1x1" width="16px" height="16px" />
-		En
-	{/if}
-{/snippet}
-
-<!-- lang toggle -->
-{#snippet langToggleGroup()}
-	<ToggleGroup
-		variant="outline"
-		type="single"
-		value={getLocale()}
-		onValueChange={(value) => setLocale(value as Locale)}
-	>
-		<!-- english -->
-		<ToggleGroupItem disabled={getLocale() == "en"} class="hover:cursor-pointer" value={"en"} aria-label={"English"}>
-			{@render langOption("en")}
-		</ToggleGroupItem>
-
-		<!-- español -->
-		<ToggleGroupItem disabled={getLocale() == "es"} class="hover:cursor-pointer" value={"es"} aria-label={"Español"}>
-			{@render langOption("es")}
-		</ToggleGroupItem>
-	</ToggleGroup>
 {/snippet}
 
 {#snippet desktopNavbar()}
