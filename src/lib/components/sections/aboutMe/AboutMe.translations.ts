c17df10fde82dea300582ae3e46c951ba799efa6
guido98q <guido98q@gmail.com>
2026-01-21 03:55:01 -0300

    todo aboutme translation

--- src/lib/components/sections/aboutMe/AboutMe.translations.ts
+++ src/lib/components/sections/aboutMe/AboutMe.translations.ts
@@ -9,12 +9,11 @@ const AboutMeDic: I_TranslationsDic<I_TraKeyAboutMe> = {
 	en: {
 		[I_TraKeyAboutMe.title]: "About me",
 		[I_TraKeyAboutMe.blob]:
-			"I'm Guido, a Full Stack Developer with years of experience creating web applications. I specialize in the React/JS environment, and am passionate about clean code, intuitive UI/UX, and efficient development workflows.",
+			"I'm Guido, Full Stack Developer with years of experience creating web applications. I specialize in the React/JS environment, and am passionate about clean code, intuitive UI/UX, and efficient development.",
 	},
 	es: {
         [I_TraKeyAboutMe.title]: "Sobre mí",
-        // !TODO
-		[I_TraKeyAboutMe.blob]: "",
+		[I_TraKeyAboutMe.blob]: "Soy Guido, desarrollador fullstack con años de experiencia. Me especializo en el ambiente React/JS, y me apasionan el codigo limpio, UI/UX intuitivas, y el desarrollo eficiente.",
 	},
 };
 
